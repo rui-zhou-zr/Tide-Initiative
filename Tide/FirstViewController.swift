5afeb46cc2812f15d4322f3eab0922769d81e0e1
Rui Zhou <ruizhouzr@berkeley.edu>
2019-01-07 16:38:06 +0800

    Initial Commit

---
+++ Tide/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tide
+//
+//  Created by zhou_rui on 1/7/19.
+//  Copyright © 2019 Rui Zhou. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
